145bb36251bc4aa5677ba9350a0389572f968a9d
tony <aamoesi@gmail.com>
2019-10-01 22:01:48 -0400

    pele

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,35 @@
 //Seeding of rando people from the internet
 
 let friendsArray = [
+    
+    {
+        name: 'Pele',
+        photo: 'https://i.dailymail.co.uk/i/pix/2016/09/17/08/38753ACD00000578-3793928-image-a-41_1474095868380.jpg',
+        scores: [
+            '2',
+            '1',
+            '4',
+            '3',
+            '2',
+            '5',
+            '4',
+            '2',
+            '2',
+            '5'
+        ]
+    },
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     {
         name: 'Anna',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Anna_Kendrick_March_22%2C_2014_%28cropped%29.jpg/330px-Anna_Kendrick_March_22%2C_2014_%28cropped%29.jpg',
