608ff57777c246369e750cc08f08392aea31e29a
tony <aamoesi@gmail.com>
2019-10-01 21:44:09 -0400

    friends.js demo

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+//Seeding of rando people from the internet
+
+let friendsArray = [
+    {
+        name: 'Anna',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Anna_Kendrick_March_22%2C_2014_%28cropped%29.jpg/330px-Anna_Kendrick_March_22%2C_2014_%28cropped%29.jpg',
+        scores: [
+            '3',
+            '2',
+            '4',
+            '2',
+            '3',
+            '1',
+            '2',
+            '4',
+            '2',
+            '3'
+        ]
+    },
+    {
+        name: 'Bob',
+        photo: 'https://blogs-images.forbes.com/cherylsnappconner/files/2014/10/robert-johnson_-1030x685.jpg',
+        scores: [
+            '2',
+            '1',
+            '4',
+            '3',
+            '2',
+            '5',
+            '4',
+            '2',
+            '2',
+            '5'
+        ]
+    },
+    {
+        name: 'Lily',
+        photo: 'https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        scores: [
+            '2',
+            '4',
+            '3',
+            '1',
+            '2',
+            '3',
+            '5',
+            '3',
+            '1',
+            '2'
+        ]
+    }
+
+]
+
+module.exports = friendsArray;
